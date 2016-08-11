ebecb19f80f572616455b1067acf8d7e8cf0510a
Sergiy Ignatenko <grydinywka@gmail.com>
2016-08-11 17:25:25 +0300

    Making popup page the same as image

--- static/js/main.js
+++ static/js/main.js
@@ -2,13 +2,17 @@ function initBuyModal() {
     $('a.add-to-cart').click(function(event) {
         var modal = $('#myModal');
         var img = $(this).parent().find('img').clone();
-        var header = $(this).parent().find('h4').clone();
+        var header = $(this).parent().find('h4 strong').clone();
         var price = $(this).parent().find('p span.big-price').clone();
 
-        modal.find('.img').html(img);
-        modal.find('.h4').html(header);
+        modal.find('.img-modal').html(img);
+        modal.find('h4').text(header.text());
         modal.find('#model-price').html(price);
-        modal.modal('show');
+        modal.modal({
+            'keyboard': false,
+//            'backdrop': false,
+            'show': true
+        });
         return false;
     });
 }
